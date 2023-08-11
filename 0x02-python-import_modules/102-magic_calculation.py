59a85ea3ef4da50e35958ce2c2b40376019a4fed
OkumuP <okumupita@gmail.com>
2023-08-11 12:41:35 -0700

    An update to the 102-magic_calculations.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -7,9 +7,8 @@ def magic_calculation(a, b):
 
     if a < b:
         c = add(a, b)
-        for i in range(4, 6):
+        for i in range(4, 7):
             c = add(c, i)
-        return (c)
-
+        return c
     else:
-        return(sub(a, b))i
+        return sub(a, b)
