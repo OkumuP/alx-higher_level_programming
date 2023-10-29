9c13d09e7c6ae21bddca7b89bdea385f671349f0
OkumuP <okumupita@gmail.com>
2023-10-29 08:15:33 -0700

    SQL more queries update

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,4 @@
+--creates the table id_not_null on your MySQL server
 CREATE TABLE IF NOT EXISTS unique_id
        (id INT DEFAULT 1,
        UNIQUE (ID),
--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
+--creates the table unique_id on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
        id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
