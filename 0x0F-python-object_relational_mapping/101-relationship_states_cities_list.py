d725936f2d8709f00257a055d62975c74871ccf7
OkumuP <okumupita@gmail.com>
2023-11-19 10:08:07 -0800

    101 fix

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa
+""" 
+lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa
 """
 import sys
 from relationship_state import Base, State
