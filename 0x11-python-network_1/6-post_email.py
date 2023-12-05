98c1941c3fe7b9de1a7bee9ae74007377217c37e
OkumuP <okumupita@gmail.com>
2023-12-05 15:38:16 -0800

    networking 1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A script that takes in a URL
+Sends a POST request to a URL with an email as a parameter and displays
+the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    payload = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=payload)
+    print(r.text)
